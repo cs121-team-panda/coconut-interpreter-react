ebbb9965c56a3719a74e770db9eaf01383737b56
Natalie Kadonaga <nat.kadonaga@gmail.com>
2018-03-28 20:49:12 -0700

    Use content-type JSON to accommodate compile args

--- src/api.js
+++ src/api.js
@@ -1,12 +1,14 @@
 import request from './utils/request';
-import toFormData from './utils/payload';
 
 const baseUrl = process.env.REACT_APP_API_BASE || 'http://localhost:5000';
 
 const run = payload =>
   request(`${baseUrl}/coconut`, {
     method: 'POST',
-    body: toFormData(payload),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
   });
 
 export default run;

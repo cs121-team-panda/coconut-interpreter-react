a46d16ba8a37896e402df793fee05dbb846d7842
Brittany <bw943600@gmail.com>
2018-04-05 01:04:46 -0700

    fixed spacing

--- src/components/Output.js
+++ src/components/Output.js
@@ -4,12 +4,12 @@ import React, { Component } from 'react';
 import AceEditor from 'react-ace';
 import type { EditorProps } from 'react-ace';
 import { withStyles } from 'material-ui/styles';
-
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import { FormControlLabel } from 'material-ui/Form';
 import Checkbox from 'material-ui/Checkbox';
+
 import 'brace/mode/text';
 import 'brace/mode/python';
 import 'brace/theme/chrome';

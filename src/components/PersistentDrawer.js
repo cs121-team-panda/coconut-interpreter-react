f05c3bae96c6f7cde2bea0ec651113a4a0219a53
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2018-04-11 10:47:27 -0700

    Fix flow error

--- src/components/PersistentDrawer.js
+++ src/components/PersistentDrawer.js
@@ -225,7 +225,9 @@ class PersistentDrawer extends React.Component<Props, State> {
     const file = new Blob([code], { type: 'text/plain' });
     element.href = URL.createObjectURL(file);
     element.download = 'coconut.coco';
-    document.body.appendChild(element);
+    if (document.body != null) {
+      document.body.appendChild(element);
+    }
     element.click();
 
     // Timeout to prevent the element being removed too soon in some browsers like Firefox
